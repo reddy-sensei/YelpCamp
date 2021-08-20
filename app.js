7e370a6e994a14caab18bc40c427ccdbc60d55f1
Nitishroboy <82563217+reddy-sensei@users.noreply.github.com>
2021-08-20 12:09:27 +0530

    some changes

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/luffy99/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
