abff966fca4701fdb96c81c2fad98293a4651667
Nitishroboy <82563217+reddy-sensei@users.noreply.github.com>
2021-08-20 11:59:38 +0530

    some changes

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongodb-session")(session);
 
-const dbUrl =  process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl =   'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
@@ -74,7 +74,7 @@ const sessionConfig = {
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        secure: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
